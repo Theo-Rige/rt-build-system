98be994a0042629ffec1f7c6e922fb794efcd4fa
Théo Rigé <rige.theo@gmail.com>
2025-06-07 17:05:04 +0200

    update(gulp): enable module option to terser

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,24 +1,24 @@
-import { src, dest, watch, series, parallel } from "gulp";
-import sassCompiler from "gulp-sass";
-import * as sassPkg from "sass";
-import terser from "gulp-terser";
-import rename from "gulp-rename";
-import sourcemaps from "gulp-sourcemaps";
-import autoprefixer from "gulp-autoprefixer";
+import { src, dest, watch, series, parallel } from 'gulp'
+import sassCompiler from 'gulp-sass'
+import * as sassPkg from 'sass'
+import terser from 'gulp-terser'
+import rename from 'gulp-rename'
+import sourcemaps from 'gulp-sourcemaps'
+import autoprefixer from 'gulp-autoprefixer'
 
-const sass = sassCompiler(sassPkg);
+const sass = sassCompiler(sassPkg)
 
 const paths = {
-  css: {
-    src: "assets/scss/**/*.scss",
-    dest: "assets/css/",
-  },
-  js: {
-    src: ["assets/js/*.js", "!assets/js/*.min.js", "!assets/js/*.map"], // Prevents an infinite loop.
-    dest: "assets/js/",
-  },
-  maps: "/", // To save the maps files in the same folder as the original files.
-};
+	css: {
+		src: 'assets/scss/**/*.scss',
+		dest: 'assets/css/'
+	},
+	js: {
+		src: ['assets/js/*.js', '!assets/js/*.min.js', '!assets/js/*.map'], // Prevents an infinite loop.
+		dest: 'assets/js/'
+	},
+	maps: '/' // To save the maps files in the same folder as the original files.
+}
 
 /**
  * Compiles SCSS files to CSS.
@@ -26,13 +26,13 @@ const paths = {
  * @returns {Stream} A Gulp stream that completes the SCSS compilation process.
  */
 function compileSCSS() {
-  return src(paths.css.src)
-    .pipe(sourcemaps.init())
-    .pipe(sass({ style: "compressed" }).on("error", sass.logError))
-    .pipe(autoprefixer())
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(sourcemaps.write(paths.maps))
-    .pipe(dest(paths.css.dest));
+	return src(paths.css.src)
+		.pipe(sourcemaps.init())
+		.pipe(sass({ style: 'compressed' }).on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(sourcemaps.write(paths.maps))
+		.pipe(dest(paths.css.dest))
 }
 
 /**
@@ -41,21 +41,21 @@ function compileSCSS() {
  * @returns {Stream} A stream containing the processed JavaScript files.
  */
 function compressJS() {
-  return src(paths.js.src)
-    .pipe(sourcemaps.init())
-    .pipe(terser())
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(sourcemaps.write(paths.maps))
-    .pipe(dest(paths.js.dest));
+	return src(paths.js.src)
+		.pipe(sourcemaps.init())
+		.pipe(terser({ module: true }))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(sourcemaps.write(paths.maps))
+		.pipe(dest(paths.js.dest))
 }
 
 /**
  * Watches for changes in CSS and JS source files.
  */
 function watchFiles() {
-  watch(paths.css.src, compileSCSS);
-  watch(paths.js.src, compressJS);
+	watch(paths.css.src, compileSCSS)
+	watch(paths.js.src, compressJS)
 }
 
-export const build = parallel(compileSCSS, compressJS);
-export default series(parallel(compileSCSS, compressJS), watchFiles);
+export const build = parallel(compileSCSS, compressJS)
+export default series(parallel(compileSCSS, compressJS), watchFiles)
