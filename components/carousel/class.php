434066433accd8d9ecff795acbf14b0f4ae6e0d1
Théo Rigé <rige.theo@gmail.com>
2025-06-29 23:10:33 +0200

    feat(carousel): add carousel component with class, XML configuration, and assets

---
+++ components/carousel/class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Carousel component class file.
+ *
+ * @package RT_Build_System
+ */
+
+namespace RTBS;
+
+if (! defined('ABSPATH')) exit;
+
+require_once RTBS_PLUGIN_DIR . 'includes/component.php';
+
+/**
+ * Carousel component class.
+ */
+class Carousel extends Component {
+    /**
+     * Component name.
+     */
+    const NAME = 'carousel';
+}
